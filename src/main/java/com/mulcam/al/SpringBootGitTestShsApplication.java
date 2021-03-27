1ce384d0ee181624de61167fd8b8e2fe0188ce91
hyun sook <hphk@cmail.com>
2021-03-27 14:30:32 +0900

    [신현숙]첫프로젝트구성

---
+++ src/main/java/com/mulcam/al/SpringBootGitTestShsApplication.java
@@ -0,0 +1,13 @@
+package com.mulcam.al;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitTestShsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitTestShsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mulcam/al/SpringBootGitTestShsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulcam.al;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitTestShsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
